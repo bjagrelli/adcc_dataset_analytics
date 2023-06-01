e074f7b54743b9fea37630636d6d9ae49e1635f8
Breno Jones <bjagrelli@gmail.com>
2023-06-01 16:04:09 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# BJJ Data Scraping
+
+
+> Data scraping from BJJ Heroes for further data analysis on BJJ statistics.
+
+### What's next
+
+There's still much to be done. But we'll get there.
+- [x] Web scraping (Extract)
+- [ ] Data cleansing (Transform) 
+- [ ] Data Ingestion (Load)
+- [ ] Data Analysis
+- [ ] Data Visualization
+
+## 🚀 Running bjj_data_scraping
+
+Before you run the scripts, make sure you have a virtual environment with the nedeed libraries installed:
+
+Create virtual environment:
+```
+python -m venv .venv
+```
+
+Install libraries:
+```
+pip install -r requirements.txt
+```
+
+To download the data from BJJ Heroes, run the command below:
+
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,35 @@
+from functions import *
+from WebScraper import WebScraper
+
+# Variables
+main_page_url = 'https://www.bjjheroes.com/a-z-bjj-fighters-list'
+fighter_page_url = 'https://www.bjjheroes.com/bjj-fighters{}'
+
+def main():
+    
+    main_page_html = get_page_html(main_page_url)
+
+    print('>>> Beginning of process\n')
+
+    soup = main_page_scraper(main_page_html)
+
+    print('>>> Getting page links for each athlete\n')
+    
+    fighters_links = get_fighters_page_link(soup, fighter_page_url)
+
+    t1 = datetime.now()
+    scraper = WebScraper(urls = fighters_links)
+
+    print(f'>>> Downloaded completed')
+    print(f'>>> Data scraping started')
+
+    fighters_list = fighters_page_scraper_html(scraper.master_dict, fighters_links)
+
+    t2 = datetime.now()
+    scraping_time = t2 - t1
+    print(f'>>> Data scraping took {scraping_time.seconds} seconds')
+
+    return fighters_list
+
+if __name__ == '__main__':
+    main()
